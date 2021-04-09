aa5a15af246809cf638be45fbbc31fab2f0dff22
Bakyt <djumabaevb@gmail.com>
2021-04-09 16:04:24 +0600

    korona peak showing on map

--- app/src/main/java/com/example/mymapsapp/MapsActivity.java
+++ app/src/main/java/com/example/mymapsapp/MapsActivity.java
@@ -39,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+        LatLng korona = new LatLng(42.5123189,74.5636513);
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(korona).title("Peak Korona"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(korona, 12));
     }
 }
